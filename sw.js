a6711df7a22e76f3588bc89da466aa8b685b84c9
Ilha-Entertainment <ilhaentertainment@gmail.com>
2026-05-03 14:03:31 -0300

    Refactor service worker to handle fetch and activate events

--- sw.js
+++ sw.js
@@ -1,21 +1,17 @@
 const CACHE_NAME = "ilha-control-v1";
-const URLS_TO_CACHE = [
-  "/p/painel.html",
-  "/p/painel.html"
-];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch("/p/painel.html").catch(() => caches.match("/p/painel.html"))
+    );
+  }
 });
